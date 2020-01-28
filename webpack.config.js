e84dfc99ace2218606965652108e4bc71e45bc5f
Alireza <alireza.mh@gmail.com>
2020-01-28 21:39:50 +0330

    chore: add clean webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 /* variables */
 /* global process __dirname module */
@@ -62,6 +63,7 @@ module.exports = {
         useShortDoctype: true,
       },
     }),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       disable: !isProduction,
       filename: "[name][hash].css",
